0633052387e05464db1930f9b9201cda09836c5c
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-11 15:29:16 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,15 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  # sql = <<-SQL
-  # CREATE TABLE IF NOT EXISTS artists (
-  # id INTEGER PRIMARY KEY,
-  # name TEXT,
-  # genre TEXT,
-  # age INTEGER,
-  # hometown TEXT
-  # )
-  # SQL
- 
-  # ActiveRecord::Base.connection.execute(sql)
+
   def change
     create_table :students do |s|
     end
