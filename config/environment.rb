c831c87109ff3d36f9615328d3e7459a3fb71b80
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-11 15:21:40 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -22,12 +22,3 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
-
-require_relative "../artist.rb"
-
-
